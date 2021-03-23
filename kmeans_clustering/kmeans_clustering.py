b88ad8fb75c165e8b47004ffec1a36d0bdd77687
Edward Brash <brash@jlab.org>
2021-03-23 15:59:33 -0400

    PyCharm update

--- kmeans_clustering/kmeans_clustering.py
+++ kmeans_clustering/kmeans_clustering.py
@@ -12,13 +12,14 @@ case = 2
 if case == 1:
     # Case 1:  Get some 2D data from sklearn
     from sklearn.datasets import make_blobs
-    features, true_labels = make_blobs(n_samples=300,centers=k,cluster_std=2.75,random_state=42)
+    features, true_labels = make_blobs(n_samples=300,centers=k,cluster_std=1.75,random_state=42)
     if debug: print (true_labels)
     df = pd.DataFrame(features, columns = ['X','Y'])
 else:
     if case == 2:
         # Case 2:  Produce some 3D spheres filled with datapoints
         features = np.zeros((500,3))
+        true_labels = np.zeros(500)
         import random
         xc = np.array([[0,0,0],[1,1,1],[-1,-1,-1],[1,-1,-1],[-1,1,1]])
 
@@ -30,8 +31,11 @@ else:
                 z = random.uniform(-radius,radius)
                 v = np.array([xc[i][0]+x,xc[i][1]+y,xc[i][2]+z])
                 index = 5*j+i
+                true_labels[index]=i
                 features[5*j+i] = v
         df = pd.DataFrame(features, columns=['X', 'Y','Z'])
+
+if debug: print (true_labels)
 
 xcenter_index, sse = cluster_kmeans(df,k)
 df['Index'] = xcenter_index
